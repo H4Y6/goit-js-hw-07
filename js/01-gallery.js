1939e760a433b3e85027a52bfdfbe01381dc7c4c
H4Y6 <89087786+H4Y6@users.noreply.github.com>
2022-01-23 18:39:03 +0200

    extended gallery & lightbox added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const markupGallery = composeMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryRef.innerHTML = markupGallery;
+galleryRef.addEventListener('click', selectImg);
+
+function selectImg(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const onEscape = (evt) => {
+    if (evt.key === 'Escape') {
+      instance.close();
+    }
+  };
+
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}"/> `,
+    {
+      onShow: (instance) => {
+        window.addEventListener('keydown', onEscape);
+      },
+      onClose: (instance) => {
+        window.removeEventListener('keydown', onEscape);
+      },
+    }
+  );
+
+  instance.show();
+}
+
+function composeMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+  </div>
+  `;
+    })
+    .join('');
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const markupGallery = composeMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryRef.innerHTML = markupGallery;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function composeMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt title="${description}" />
+      </a>
+      `;
+    })
+    .join('');
+}
